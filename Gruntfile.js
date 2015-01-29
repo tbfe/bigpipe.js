ad0e8b5523052970b7eb070aed668e754a46c421
leeyeh <lee55962698@gmail.com>
2015-01-29 18:20:19 +0800

    add .editorconfig.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,69 +1,69 @@
 module.exports = function(grunt) {
-	require('load-grunt-tasks')(grunt);
+    require('load-grunt-tasks')(grunt);
 
-	var browsers = [{
-		browserName: "chrome",
-		platform: "XP"
-	}, {
-		browserName: "chrome",
-		platform: "XP",
-		version: "30"
-	}, {
-		browserName: "chrome",
-		platform: "OS X 10.10"
-	}, {
-		browserName: "internet explorer",
-		platform: "XP",
-		version: "8"
-	}, {
-		browserName: "internet explorer",
-		platform: "WIN7",
-		version: "9"
-	}, {
-		browserName: "internet explorer",
-		version: "10"
-	}, {
-		browserName: "internet explorer",
-		version: "11"
-	}, {
-		browserName: "firefox"
-	}, {
-		browserName: "safari",
-		platform: "OS X 10.10"
-	}, {
-		browserName: "iphone",
-		version: "8.1"
-	}, {
-		browserName: "android",
-		version: "4.4"
-	}];
+    var browsers = [{
+        browserName: "chrome",
+        platform: "XP"
+    }, {
+        browserName: "chrome",
+        platform: "XP",
+        version: "30"
+    }, {
+        browserName: "chrome",
+        platform: "OS X 10.10"
+    }, {
+        browserName: "internet explorer",
+        platform: "XP",
+        version: "8"
+    }, {
+        browserName: "internet explorer",
+        platform: "WIN7",
+        version: "9"
+    }, {
+        browserName: "internet explorer",
+        version: "10"
+    }, {
+        browserName: "internet explorer",
+        version: "11"
+    }, {
+        browserName: "firefox"
+    }, {
+        browserName: "safari",
+        platform: "OS X 10.10"
+    }, {
+        browserName: "iphone",
+        version: "8.1"
+    }, {
+        browserName: "android",
+        version: "4.4"
+    }];
 
-	grunt.initConfig({
-		connect: {
-			server: {
-				options: {
-					base: "",
-					port: 9999
-				}
-			}
-		},
-		'saucelabs-jasmine': {
-			all: {
-				options: {
-					urls: ["http://127.0.0.1:9999/test/index.html"],
-					tunnelTimeout: 5,
-					build: process.env.TRAVIS_JOB_ID,
-					concurrency: 3,
-					browsers: browsers,
-					testname: "bigpipe.js tests",
-					tags: ["ci", "master"]
-				}
-			}
-		},
-		watch: {}
-	});
+    grunt.initConfig({
+        connect: {
+            server: {
+                options: {
+                    base: "",
+                    port: 9999
+                }
+            }
+        },
+        'saucelabs-jasmine': {
+            all: {
+                options: {
+                    urls: ["http://127.0.0.1:9999/test/index.html"],
+                    tunnelTimeout: 5,
+                    build: process.env.TRAVIS_JOB_ID,
+                    concurrency: 3,
+                    browsers: browsers,
+                    testname: "bigpipe.js tests",
+                    tags: ["ci", "master"]
+                }
+            }
+        },
+        watch: {}
+    });
 
 
-	grunt.registerTask("dev", ["connect", "watch"]);
-	grunt.registerTask("test", ["connect", "saucelabs-jasmine"]);
+    grunt.registerTask("dev", ["connect", "watch"]);
+    grunt.registerTask("test", ["connect", "saucelabs-jasmine"]);
 };
--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-node test/serve & SERVER_PID=$!;
-phantomjs test/runner.js test/index.html;
-result=$?;
-kill $SERVER_PID;
-exit $result;
