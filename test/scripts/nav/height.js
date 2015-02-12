fe9760fda19d1ea913f9d2fcb3746c650cc99b60
leeyeh <lee55962698@gmail.com>
2015-02-12 14:04:14 +0800

    Fix a test bug.
    Since styles and scripts are loaded in parallel, scripts should not depend on styles.

--- test/scripts/nav/height.js
+++
@@ -1,4 +0,0 @@
-var nav = document.getElementById('nav');
-var height = window.getComputedStyle(nav).height;
-window.navHeight = Number(height.split('px')[0]);
-console.log('height.js executied');
---
+++ test/scripts/nav/mod_a.js
@@ -0,0 +1,2 @@
+window.modA = 3;
+console.log('mod_A.js executied');
